613cecd1336f84585e95a16e4ce7040ca679d49e
Guzel_Ayupova <guzel_robertovna@mail.ru>
2023-05-30 23:08:09 +0500

    Задача47. Программа заполнения массива вещественными числами

---
+++ task47/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 47: Задайте двумерный массив размером m×n,
+// заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+
+int rows = Prompt("Введите количество строк массива: ");
+int columns = Prompt("Введите количество столбцов массива: ");
+double[,] array = new double[rows, columns];
+FillArray(array, rows, columns);
+PrintArray(array);
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine()!;
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+void FillArray(double[,] matrix, int m, int n)
+{
+    Random random = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = random.NextDouble() * 100;
+        }
+    }
+}
+
+void PrintArray(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($" {Math.Round(matrix[i, j], 2)}  ");
+        }
+        Console.WriteLine();
+
+    }
+}
